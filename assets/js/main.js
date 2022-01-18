8e0296a955e83c73a508ccbe184ed76b3ab787e8
Manoj Kumar Thapa <genjithapa182@hotmail.com>
2022-01-18 10:06:32 +0000

    added show all location

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
     }
 
     function resultPageShowAll(num){
-      $('#result').html(``);
+      $('#result').html();
       pageLimit(dataNumber);
       dataArray[num].forEach(function(item){
         $('#result').append(`
@@ -59,7 +59,7 @@ $(document).ready(function(){
         </div>
         `)
         $('#pageNumber').html(`
-        page: ${pageNumber + 1} / ${pageLimitNumber}
+        page: ${pageNumber + 1} of ${pageLimitNumber}
         `)
         if(pageNumber == 0){
           console.log('page number is 1')
@@ -147,7 +147,7 @@ $(document).ready(function(){
               </div>
               `)
               $('#pageNumber').html(`
-              page: ${pageNumberDept + 1} / ${pageNumberLimitDept}
+              page: ${pageNumberDept + 1} of ${pageNumberLimitDept}
               `)
               if(pageNumberDept == 0){
                 $('.fa-chevron-circle-left').css('visibility', 'hidden');
@@ -175,6 +175,86 @@ $(document).ready(function(){
         }
       })
     })
+
+
+    // Show all Locations
+    $('#showLocations').click(function(){
+      $('#result').html(``);
+      $('.functionality').toggle('is-active');
+      let pageArrayLocation = [];
+      let dataArrayLocation = [];
+      let pageNumberLocation = 0;
+      let pageNumberLimitLocation = 0;     
+      $.ajax({
+        url: 'assets/php/getAllLocations.php',
+        success: function(item){
+          let data = item.data;
+          let dataNumberLocation = data.length;
+          for(let i = 0; i<=dataNumberLocation - 1; i++){
+            if(i % 6 !=0 || i== 0){
+              pageArrayLocation.push(data[i]);
+              if(i == dataNumberLocation -1){
+                dataArrayLocation.push(pageArrayLocation);
+                pageArrayLocation = [];
+              }
+            }else{
+              dataArrayLocation.push(pageArrayLocation);
+              pageArrayLocation = [];
+              pageArrayLocation.push(data[i]);
+            }
+          }
+            function pageLimit(num){
+              pageNumberLimitLocation = num / 6;
+              if(!Number.isInteger(pageNumberLimitLocation)){
+                pageNumberLimitLocation += 1;
+                pageNumberLimitLocation = parseInt(pageNumberLimitLocation);
+            }
+          }
+          function resultPageAllLocation(num){
+            $('#result').html(``);
+            pageLimit(dataNumberLocation);    
+            dataArrayLocation[num].forEach(function(item){
+              $('#result').append(`
+              <div class="card">
+                <div class="info">
+                  <h3>${item.name}</h3>
+                </div>
+              </div>
+              `)              
+            })  
+            $('#pageNumber').html(`
+            page: ${pageNumberLocation + 1} of ${pageNumberLimitLocation}
+            `)
+            if(pageNumberLimitLocation == 0){
+              $('.fa-chevron-circle-left').css('visibility', 'hidden');
+            }
+            else{
+              $('.fa-chevron-circle-left').css('visibility', 'visible');
+            }
+            if(pageNumberLimitLocation == pageNumberLimitLocation -1){
+              $('.fa-chevron-circle-right').css('visibility', 'hidden');
+            }
+            else{
+              $('.fa-chevron-circle-right').css('visibility', 'visible');
+            } 
+            if(pageNumberLocation == 0){
+              $('.fa-chevron-circle-left').css('visibility', 'hidden');
+              $('.fa-chevron-circle-right').css('visibility', 'hidden');
+            }
+
+          }
+          resultPageAllLocation(pageNumberLocation);
+          $('.fa-chevron-circle-left').click(function(){
+            pageNumberLocation -= 1;
+            resultPageAllLocation(pageNumberLocation);
+          })
+          $('.fa-chevron-circle-right').click(function(){
+            pageNumberLocation += 1;
+            resultPageAllLocation(pageNumberLocation);
+          })
+        }
+      })
+    })
   }
 })
 })
@@ -183,4 +263,4 @@ $(document).ready(function(){
 
 
 
-console.log('omg221122');
+console.log('oh');
